4ea68921f5d0963a9721f5fdd5b5cf0cc7377836
guptaguruprakash <166430475+guptaguruprakash@users.noreply.github.com>
2024-08-31 21:22:04 +0545

    Update pro3.cpp
    question no3 added

--- guru prakash gupta_pur080bct032/pro3.cpp
+++ guru prakash gupta_pur080bct032/pro3.cpp
@@ -1,3 +1,8 @@
+/*  PROBLEM 3.
+Define a class Library with bookTitle, author, and numberOfCopies as data
+members. Implement methods to add new books, issue books, and return books.
+Ensure that the number of copies is appropriately updated for each operation.
+*/
 #include <iostream>
 using namespace std;
 
